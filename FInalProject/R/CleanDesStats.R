bab62b507110f7049b4b85d170ff5d883fc6dd8c
blaira1 <77460845+blaira1@users.noreply.github.com>
2021-04-24 16:44:04 -0400

    Delete FInalProject directory

--- FInalProject/R/CleanDesStats.R
+++
@@ -1,8 +0,0 @@
-#RScript that runs functions from AllFUnctions.R. Also cleans data and adds to dataframe. Runs summary function
-
-data <- cleandata(data)
-data$finalplacing <- factor(data$POSTSEASON,labels=c("2","1","4","3","6","7","8","5"))
-data$teamrating <- teamrating(data$ADJOE, data$ADJDE, data$BARTHAG)
-data$winrate <- winrate(data$W, data$G)
-
-summary(data)
